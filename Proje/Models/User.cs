90085e635f98783d4a95e4e729501dcef602a1e2
ccanefe <can.efe@ogr.sakarya.edu.tr>
2023-12-27 19:31:15 +0300

    model olusturuldu

---
+++ Proje/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Proje.Models
+{
+    public class User
+    {   
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı boş geçilemez!")]
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı boş geçilemez!")]
+        [Display(Name = "Soyad")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Telefon numarası alanı boş geçilemez!")]
+        [Display(Name = "Telefon Numarası")]
+        public string TelNo { get; set; }
+
+        [Required(ErrorMessage = "TC kimlik numarası alanı boş geçilemez!")]
+        [Display(Name = "TC kimlik numarası")]
+        public string TcNo { get; set; }
+
+        [Required(ErrorMessage = "Email alanı boş geçilemez!")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+    }
+}
