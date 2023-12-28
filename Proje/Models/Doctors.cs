c8d070dc475b316fbf58c4f88d41d959657fbabc
ccanefe <can.efe@ogr.sakarya.edu.tr>
2023-12-28 16:14:21 +0300

    degisiklik uygulandi

---
+++ Proje/Models/Doctors.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proje.Models
+{
+    public class Doctors
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı boş geçilemez!")]
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı boş geçilemez!")]
+        [Display(Name = "Soyad")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Telefon numarası alanı boş geçilemez!")]
+        [Display(Name = "Telefon Numarası")]
+        public string TelNo { get; set; }
+
+        [Required(ErrorMessage = "TC kimlik numarası alanı boş geçilemez!")]
+        [Display(Name = "TC kimlik numarası")]
+        public string TcNo { get; set; }
+
+        [Required(ErrorMessage = "Email alanı boş geçilemez!")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
